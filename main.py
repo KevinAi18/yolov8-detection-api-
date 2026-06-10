5b5ce08d0f493e5b95dc07d130471307dde3b16b
KevinAi18 <KevinAi18@users.noreply.github.com>
2026-06-10 08:09:42 +0530

    feat: add JSON and image response endpoints for real-time object detection

--- main.py
+++ main.py
@@ -121,5 +121,58 @@ def crop_image_by_predict(image: Image, predict: pd.DataFrame(), crop_class_name
     return(img_crop)
 
 
+######################### MAIN Func #################################
 
 
+@app.post("/img_object_detection_to_json")
+def img_object_detection_to_json(file: bytes = File(...)):
+    """
+    Object Detection from an image.
+
+    Args:
+        file (bytes): The image file in bytes format.
+    Returns:
+        dict: JSON format containing the Objects Detections.
+    """
+    # Step 1: Initialize the result dictionary with None values
+    result={'detect_objects': None}
+
+    # Step 2: Convert the image file to an image object
+    input_image = get_image_from_bytes(file)
+
+    # Step 3: Predict from model
+    predict = detect_sample_model(input_image)
+
+    # Step 4: Select detect obj return info
+    # here you can choose what data to send to the result
+    detect_res = predict[['name', 'confidence']]
+    objects = detect_res['name'].values
+
+    result['detect_objects_names'] = ', '.join(objects)
+    result['detect_objects'] = json.loads(detect_res.to_json(orient='records'))
+
+    # Step 5: Logs and return
+    logger.info("results: {}", result)
+    return result
+
+@app.post("/img_object_detection_to_img")
+def img_object_detection_to_img(file: bytes = File(...)):
+    """
+    Object Detection from an image plot bbox on image
+
+    Args:
+        file (bytes): The image file in bytes format.
+    Returns:
+        Image: Image in bytes with bbox annotations.
+    """
+    # get image from bytes
+    input_image = get_image_from_bytes(file)
+
+    # model predict
+    predict = detect_sample_model(input_image)
+
+    # add bbox on image
+    final_image = add_bboxs_on_img(image = input_image, predict = predict)
+
+    # return image in bytes format
+    return StreamingResponse(content=get_bytes_from_image(final_image), media_type="image/jpeg")
